7c2226ea896f2dca5f2907e4a1d2133c4ea40595
QVanbutsele <vanbutsele@intechinfo.fr>
2016-04-18 11:00:19 +0200

    Initalize the test branche and create the test plateform for the GameCore

---
+++ ITI.GameCore.Test/Tests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.GameCore.Test
+{
+    public class Tests
+    {
+        //TODO
+    }
+}
