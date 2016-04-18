0521615d579f8ed2c052ebfc7e0053c817f1c4c8
QVanbutsele <vanbutsele@intechinfo.fr>
2016-04-18 02:22:35 +0200

    Create public API and comment base

--- ITI.GameCore/Game.cs
+++ ITI.GameCore/Game.cs
@@ -8,5 +8,81 @@ namespace ITI.GameCore
 {
     public class Game
     {
+        //attributes
+        
+        //constructor        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Game"/> class.
+        /// </summary>
+        public Game()
+        {
+
+        }
+        //properties
+        public int GetWidth
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int GetHeight
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool[] GetMap
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        //Methods
+        public bool CreatePiece()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemovePiece()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MovePiece()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPiece()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPieceKilled()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckVictoryConditionOfAttacker()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckVictoryConditionOfDefender()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateTurn()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
---
+++ ITI.GameCore/Piece.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.GameCore
+{
+    class Piece
+    {
+        //attributes
+
+        //constructor
+        public Piece()
+        { }
+        //properties
+        public int Side
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            internal set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public int Type
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            internal set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        //methodes
+    }
+}
