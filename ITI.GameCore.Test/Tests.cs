084ce59804dcb11476cfecc074c9bb98f93d8e45
QVanbutsele <vanbutsele@intechinfo.fr>
2016-04-26 17:19:54 +0200

    Add package of tests

--- ITI.GameCore.Test/Tests.cs
+++ ITI.GameCore.Test/Tests.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+
 
 namespace ITI.GameCore.Test
 {
+    [TestFixture]
     public class Tests
     {
-        //TODO
+        [TestCase(10, 5)]
+        public void T01_ctor_initializes_the_game_correctly(byte width, byte height)
+        {
+            Game sut = new Game(width, height);
+
+            Assert.That(sut.GetWidth, Is.EqualTo(width));
+            Assert.That(sut.GetHeight, Is.EqualTo(height));
+        }
     }
 }
