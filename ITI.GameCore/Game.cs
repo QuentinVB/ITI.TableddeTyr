8a1c35600c8972738e09ce5771cbf840f4b9d9da
QVanbutsele <vanbutsele@intechinfo.fr>
2016-04-26 16:27:41 +0200

    Relecture API, publication de l'API et du core

--- ITI.GameCore/Game.cs
+++ ITI.GameCore/Game.cs
@@ -8,22 +8,38 @@ namespace ITI.GameCore
 {
     public class Game
     {
-        //attributes
         /*
-            dictionnary of attacker
-            dictionnary of defender
-            dictionnary of the map
-        */
+        0 : 00 : empty
+        1 : 01 : atk : attacker
+        2 : 10 : def : defensor
+        3 : 11 : king
+        */        
+        //attributes
+
+        //byte _tafl ; //NEED TEST AND DOC !
+
+        byte _width; //the size of the tafl
+        byte _height; //the size of the tafl
+        bool _atkTurn; //true if is the turn of attacker, else the turn of defendor
+        byte _defensorAlive; //amount of defensor alive
+        byte _attackerAlive;//amount of attacker alive
+
+        //collection
+        byte[,] tafl;
+         
         //constructor        
         /// <summary>
         /// Initializes a new instance of the <see cref="Game"/> class.
         /// </summary>
-        public Game(/*width, heigth, must be int*/)
+        public Game(byte width, byte height)
         {
-
+            _width = width;
+            _height = height;
+            byte[,] tafl = new byte[width,height];
+            //create default tafl
         }
         //properties
-        public int GetWidth
+        public byte GetWidth //get width of the tafl
         {
             get
             {
@@ -31,7 +47,7 @@ namespace ITI.GameCore
             }
         }
 
-        public int GetHeight
+        public byte GetHeight//get height of the tafl
         {
             get
             {
@@ -39,54 +55,57 @@ namespace ITI.GameCore
             }
         }
 
-        public bool[] GetMap
+        public byte[,] GetMap //return the int map for the UI, the capture conditions...
+        {
+            get //later here stand the mighty "byte avec tableau"
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public bool IsAtkPlaying //get the current team who play turn
         {
             get
             {
                 throw new NotImplementedException();
             }
         }
-
-        //Methods
-        public bool CreatePiece()
+        //Methods - internal
+        internal bool SetCase(byte x, byte y,int newValue)//set the state of the case
         {
             throw new NotImplementedException();
         }
 
-        public bool RemovePiece()
+        public bool CheckCapture(byte[,] thisPiece)//check the capture around this piece, then call for removing if needed
         {
             throw new NotImplementedException();
         }
 
-        public bool MovePiece()
+        //public - public
+        public bool TryMove(byte[,] thisPiece, byte x, byte y)//try of a specified piece is allowed to move to the designated position
         {
             throw new NotImplementedException();
         }
-
-        public bool IsKing()
+        public bool MovePiece(byte[,] thisPiece, byte x, byte y)//move a selected piece to a specified position
         {
             throw new NotImplementedException();
         }
-
-        public bool IsPieceKilled()
+        public byte CheckVictoryCondition(byte[,])//check if victory condition for the attacker are met (aka circling the king and his defensor) and  victory condition for defender are met (aka the king is in a forteress)
         {
             throw new NotImplementedException();
         }
-
-        public bool CheckVictoryConditionOfAttacker()
+        /*return : 
+        0 : nobody
+        1 : atk
+        2 : def         
+        */
+        public byte UpdateTurn()//called by a player during his turn, moving a piece, check capture condition, victory condition and switch to next player
         {
             throw new NotImplementedException();
         }
-
-        public bool CheckVictoryConditionOfDefender()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool UpdateTurn()
-        {
-            throw new NotImplementedException();
-        }
-
+        /*return : 
+        0 : nobody
+        1 : atk
+        2 : def         
+        */
     }
 }
