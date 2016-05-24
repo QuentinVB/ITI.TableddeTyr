883a1cc7268f1ae637250c3ec96002c9ecc733a0
QVanbutsele <vanbutsele@intechinfo.fr>
2016-05-24 12:24:37 +0200

    create interface file for sensor

---
+++ ITI.TabledeTyr.Freyja/00-IO Function/ISensor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.TabledeTyr.Freyja._00_IO_Function
+{
+    public interface ISensor
+    {
+        /*
+        Pawn this[int x, int y] { get; }
+        int AttackerCount { get; }
+        int DefenderCount { get; }
+        bool HasKing { get; }
+        int Height { get; }
+        int Width { get; }
+        */
+    }
+}
