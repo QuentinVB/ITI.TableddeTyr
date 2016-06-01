0c175684727d685e72a928e00368f33ca30004d9
QVanbutsele <vanbutsele@intechinfo.fr>
2016-06-01 11:33:15 +0200

    Simulate : ZE big loop -- node tree optimization -- document of the simulate -- create the Simulatenode

--- ITI.TabledeTyr.Freyja/01-Simulation/Simulate.cs
+++ ITI.TabledeTyr.Freyja/01-Simulation/Simulate.cs
@@ -13,81 +13,125 @@ namespace ITI.TabledeTyr.Freyja
         internal string _key;
         internal bool _isSimulatedFreyjaAtk;
         internal Game _simulatedGame;
+        internal SimulationNode root;
 
-        Dictionary<string, SimulationNode> SimulatedTree = new Dictionary<string, SimulationNode>();//dictionnary containing the tree
-        
-
+        internal Dictionary<string, SimulationNode> SimulatedTree = new Dictionary<string, SimulationNode>();//dictionnary containing the tree
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Simulate"/> class.
+        /// </summary>
+        /// <param name="ctx">The CTX.</param>
         public Simulate(Freyja_Core ctx)
         {
             _ctx = ctx;
             _isSimulatedFreyjaAtk = _ctx._Sensor._isFreyjaAtk;
-            _simulatedGame = _ctx._Sensor._game.DeedCopy();
+            _simulatedGame = _ctx._Sensor._game.DeepCopy();//COPY OF THE GAME WITH HIS TAFL !
         }
+        /// <summary>
+        /// Updates the simulation.
+        /// </summary>
         void UpdateSimulation()
         {
-            SimulationNode root = new SimulationNode(Guid.NewGuid().ToString(),_ctx._Sensor.currentTafl , 0);//create the root of the tree (getting the current state of the system)
+            root = new SimulationNode(Guid.NewGuid().ToString(),_ctx._Sensor.currentTafl , 0);//create the root of the tree (getting the current state of the system)
 
             for (int i = 0; i < _ctx._Sensor.currentTafl.Width; i++)
             {
                 for (int j = 0; j < _ctx._Sensor.currentTafl.Height; j++)
                 {
-                    if( _isSimulatedFreyjaAtk == true)
+                    PossibleMove possibleMove = _ctx.originGame.CanMove(i, j);
+                    if (possibleMove.IsFree() == true)
                     {
-                        PossibleMove possibleMove = _ctx.originGame.CanMove(i, j);
-                        if (possibleMove.IsFree() == true)
+                        if ( _isSimulatedFreyjaAtk == true)
                         {
+
                             if (_ctx._Sensor.currentTafl[i, j] == Pawn.Attacker)
                             {
                                 if(possibleMove.Up > 0)
                                 {
                                     for (int up = 0; up < possibleMove.Up; up++)
                                     {
-                                        //createnode HERE
+                                        SimulatedNode(i, j, i, up);
                                     }
                                 }
                                 if (possibleMove.Down > 0)
                                 {
                                     for (int down = 0; down < possibleMove.Up; down++)
                                     {
-                                        //createnode HERE
+                                        SimulatedNode(i, j, i, down);
                                     }
                                 }
                                 if (possibleMove.Left > 0)
                                 {
                                     for (int left = 0; left < possibleMove.Up; left++)
                                     {
-                                        //createnode HERE
+                                        SimulatedNode(i, j, left, j);
                                     }
                                 }
                                 if (possibleMove.Right > 0)
                                 {
                                     for (int right = 0; right < possibleMove.Up; right++)
                                     {
-                                        //createnode HERE
+                                        SimulatedNode(i, j, right, j);
                                     }
                                 }
                             }                            
                         }
-                    }
-                    else
-                    {
-                        if (_ctx._Sensor.currentTafl[i, j] == Pawn.Defender || _ctx._Sensor.currentTafl[i, j] == Pawn.King)
+                        else
                         {
-
+                            if (_ctx._Sensor.currentTafl[i, j] == Pawn.Defender || _ctx._Sensor.currentTafl[i, j] == Pawn.King)
+                            {
+                                if (possibleMove.Up > 0)
+                                {
+                                    for (int up = 0; up < possibleMove.Up; up++)
+                                    {
+                                        SimulatedNode(i, j, i, up);
+                                    }
+                                }
+                                if (possibleMove.Down > 0)
+                                {
+                                    for (int down = 0; down < possibleMove.Up; down++)
+                                    {
+                                        SimulatedNode(i, j, i, down);
+                                    }
+                                }
+                                if (possibleMove.Left > 0)
+                                {
+                                    for (int left = 0; left < possibleMove.Up; left++)
+                                    {
+                                        SimulatedNode(i, j, left, j);
+                                    }
+                                }
+                                if (possibleMove.Right > 0)
+                                {
+                                    for (int right = 0; right < possibleMove.Up; right++)
+                                    {
+                                        SimulatedNode(i, j, right, j);
+                                    }
+                                }
+                            }
                         }
-                    }
-                    
-
-
+                    }                    
                 }
             }
 
 
         }
-        void Movement(int x,int y,int x2,int y2)
+        /// <summary>
+        /// Simulated nodes.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="x2">The x2.</param>
+        /// <param name="y2">The y2.</param>
+        void SimulatedNode(int x,int y,int x2,int y2)
         {
             _simulatedGame.MovePawn(x, y, x2, y2);
             _simulatedGame.UpdateTurn();
+
+            Move move = new Move(x, y, x2, y2);
+
+            _key = Guid.NewGuid().ToString();//generating new key
+            SimulatedTree[_key] = new SimulationNode(_key, _simulatedGame.Tafl,0, move);//creating new node
+            root.AddChild(SimulatedTree[_key]);//linking root, designating the new created node as one of his childs 
         }
         /*
         Dictionary<string, SimulationNode> SimulatedTree = new Dictionary<string, SimulationNode>();//dictionnary containing the tree
