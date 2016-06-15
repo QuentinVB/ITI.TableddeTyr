a63557a16bcb96b66098ae266d47413d9d3a1225
Vivian Amdidouche <amdidouche@intechinfo.fr>
2016-06-15 11:44:33 +0200

    Extracted Helper.CheckRange to toolbox.

--- ITI.GameCore/GameCoreTools.cs
+++ ITI.GameCore/GameCoreTools.cs
@@ -90,4 +90,13 @@ namespace ITI.GameCore
             Y = y;
         }
     }
+    static public class Helper
+    {
+        static internal void CheckRange(int width, int height, int x, int y)
+        {
+            if (x < 0 || x > width) throw new ArgumentOutOfRangeException("Can't aim out of the tafl", nameof(x));
+            if (y < 0 || y > height) throw new ArgumentOutOfRangeException("Can't aim out of the tafl", nameof(y));
+
+        }
+    }
 }
